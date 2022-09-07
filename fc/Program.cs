fe6987f059052614f655adeeb8cf12dc4bd4ff91
SilverXuz <silver-xuz@mail.ru>
2022-09-07 22:23:55 +0300

    Сохранение кода с решением задачи

--- fc/Program.cs
+++ fc/Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿/* Финальная контрольная работа
+Задание
+Написать программу, которая из имеющегося массива строк формирует массив строк, 
+длинна которых меньше либо равна 3 символам.
+*/
+
+
+Console.Write("Введите размер массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int x = 0;
+string[] array = new string[n];
+string[] arrayX = new string[n];
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write("Введите значение массива: ");
+    var m = Console.ReadLine();
+    array[i] = m;
+    if (array[i].Length <= 3)
+    {
+        arrayX[x] = m;
+        x++;
+    }
+}
+arrayX = arrayX.Where(x => !string.IsNullOrWhiteSpace(x)).ToArray();
+Console.WriteLine("[" + string.Join(", ", array) + "]");
+Console.WriteLine("[" + string.Join(", ", arrayX) + "]");
